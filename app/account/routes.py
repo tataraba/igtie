44f077415eeaef921908978e76974178a34aae95
Mario Munoz <pythonbynight@gmail.com>
2022-09-05 18:50:36 -0700

    Use route decorator for response

--- app/account/routes.py
+++ app/account/routes.py
@@ -1,9 +1,16 @@
 # Define all routes related to authentication/login/account creation
+from functools import wraps
+
+from app.webtools.decorators import response
+from app.webtools.models import DefaultPageView
 from fastapi import APIRouter, Request
 
 user_router = APIRouter()
 
 
 @user_router.get("/")
+@response(template_file="index.html")
 async def get_home(request: Request):
-    return {"test": "complete"}
+    # view = DefaultPageView(request)
+    context = {"request": request, "hello": "goodbye"}
+    return context
