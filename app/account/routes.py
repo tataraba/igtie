b42bd5c3968c84d4ffcec4d33b088baa0e0822c1
Mario Munoz <pythonbynight@gmail.com>
2022-09-23 15:52:23 -0700

    More design changes

--- app/account/routes.py
+++ app/account/routes.py
@@ -1,18 +1,40 @@
 # Define all routes related to authentication/login/account creation
-from app.webtools.decorators import response
+# from app.webtools.decorators import response
+from app.webtools.mount import init_template
 from app.webtools.viewmodel import DefaultView
 from fastapi import APIRouter, Request
 
+t = init_template()
 user_router = APIRouter()
 
 
 @user_router.get("/")
-@response(template_file="index.html")
-async def get_home(request: Request, x="another thing"):
+async def get_home(request: Request):
     context = DefaultView()
-    return context
+    return t.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+            } | context.to_dict()
+    )
 
-@user_router.get("/home")
-@response(template_file="home.html")
-async def get_user_home(request: Request):
-    ...
+
+@user_router.get("/{nav_select}")
+async def get_user_home(request: Request, nav_select: str | None = None):
+    print(request.headers.get("HX-Request"))
+
+    if not nav_select:
+        return t.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+            }
+        )
+
+    return t.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+                "nav_select": nav_select,
+            }
+        )
